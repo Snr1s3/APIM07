e1c61978979161e65c9bf2961ad7dcc4e6f0a466
Alba Segura <2023_alba.segura@gmail.com>
2025-03-06 16:49:17 +0100

    delete

--- API/main.py
+++ API/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException, Depends
 from .client import get_db_connection
-from models import IncomeCreate, Income, ExpenseCreate, Expense
+from .models import IncomeCreate, Income, ExpenseCreate, Expense
 from datetime import date
 import mysql.connector
 
